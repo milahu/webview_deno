5c0e8dd31af274cffa84bfe14a475899b888c927
Milan Hauth <milahu@gmail.com>
2022-10-13 13:08:10 +0200

    use patched https://deno.land/x/plug

--- deps.ts
+++ deps.ts
@@ -1,1 +1,4 @@
-export { dlopen, download } from "https://deno.land/x/plug@1.0.0-rc.3/mod.ts";
+// https://github.com/denosaurs/plug/issues/20
+// download PermissionDenied. make file writable after copy to cache
+//export { dlopen, download } from "https://deno.land/x/plug@1.0.0-rc.3/mod.ts";
+export { dlopen, download } from "../plug/mod.ts";
